60053bc5fa504efd87d4e21c41cca5447981a5c3
Toni Humanes <antonio.humanes@sngular.com>
2019-06-19 17:55:39 +0200

    curso profesional app

---
+++ curso-profesional/platzi-music/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div id="formLogin" class="login">
+      <img src="./assets/img-logo.jpg">
+      <h1 class="login-form_title">{{title}}</h1>
+      <form action class="form">
+        <label for class="form__label">{{ labelName }}</label>
+        <input type="text" class="form__input">
+        <label for class="form__label">{{ labelSubname }}</label>
+        <input type="text" class="form__input">
+        <label for class="form__label">{{ labelCargo }}</label>
+        <input type="text" class="form__input">
+        <input type="submit" :value="send | uppercase" class="form__button">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "formLogin",
+
+  data() {
+    return {
+      title: "Registro estación Platzi",
+      labelName: "Nombre",
+      labelSubname: "Apellido",
+      labelCargo: "Cargo",
+      send: "Enviar"
+    };
+    data: {
+      datos: [];
+    }
+  },
+  filters: {
+    uppercase: function(str) {
+      return str.toUpperCase();
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
